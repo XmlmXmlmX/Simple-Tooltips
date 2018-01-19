2a122ced9ba4043aa802e385e76e4b08b3e812f4
Sebastian Hösl <sebastian.hoesl@tts-company.com>
2018-01-19 14:11:44 +0100

    Version 1.0.0-beta1

--- build.js
+++ build.js
@@ -1,6 +1,12 @@
-var postscss = require('postscss');
+const postscss = require('postscss');
 
-postscss([postcss plugins]).process({
-  from: 'src/main.scss',
-  to: 'dist/simple-tooltips.css'
-});
+postscss([
+    require('autoprefixer')({ browsers: 'last 2 versions' }),
+    require('cssnano')()
+]).processMany([{
+    from: 'src/main.scss',
+    to: 'dist/simple-tooltips.css'
+}, {
+    from: 'test/preview.scss',
+    to: 'test/preview.css'
+}]);
